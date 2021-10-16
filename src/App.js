d9df5814f8bb703ba188c4505f4b6ae9b97e2d3c
abdmmar <opowae909@gmail.com>
2021-10-16 21:10:13 +0700

    feat: update app and add heroicons

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React from 'react'
-import { Counter } from './features/counter/Counter'
-import './App.css'
+import { Container, Grid } from '@chakra-ui/react'
+
+import NavBar from 'components/NavBar'
+import UserList from 'features/users/UserList'
+import UserDetail from 'features/users/UserDetail'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            React
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container maxWidth="container.xl">
+      <NavBar />
+      <Grid as="main" templateColumns="20% 1fr 25%" gap="4">
+        <UserList />
+        <UserDetail />
+        <Grid></Grid>
+      </Grid>
+    </Container>
   )
 }
 
