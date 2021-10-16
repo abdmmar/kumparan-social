bf6558393c4fbcc876e1f7dae599491eb2fbc548
abdmmar <opowae909@gmail.com>
2021-10-17 00:09:01 +0700

    feat: add rtk query reducer and user reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,27 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 import { usersApi } from 'features/users/usersAPI'
+import { postsApi } from 'features/posts/postsAPI'
+import { albumsApi } from 'features/albums/albumsAPI'
+import { commentsApi } from 'features/comments/commentsAPI'
+
+import usersSlice from 'features/users/usersSlice'
 
 export const store = configureStore({
   reducer: {
+    user: usersSlice,
     [usersApi.reducerPath]: usersApi.reducer,
+    [postsApi.reducerPath]: postsApi.reducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [commentsApi.reducerPath]: commentsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(usersApi.middleware)
+    return getDefaultMiddleware().concat(
+      usersApi.middleware,
+      postsApi.middleware,
+      albumsApi.middleware,
+      commentsApi.middleware,
+    )
   },
 })
 
