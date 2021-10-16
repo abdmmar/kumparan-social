a515560a851c7e495fb95524f3e7512f91b73b2c
abdmmar <opowae909@gmail.com>
2021-10-17 00:11:35 +0700

    feat(albums): use query

--- src/features/albums/AlbumItem.js
+++ src/features/albums/AlbumItem.js
@@ -1,51 +1,16 @@
 import PropTypes from 'prop-types'
-import { Box, Grid, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react'
+import { Box, Grid, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Spinner } from '@chakra-ui/react'
 
 import AlbumThumbnail from './AlbumThumbnail'
+import { useGetPhotosQuery } from './albumsAPI'
 
 const AlbumItem = ({ id, title }) => {
-  const images = [
-    {
-      albumId: 1,
-      id: 1,
-      title: 'accusamus beatae ad facilis cum similique qui sunt',
-      url: 'https://via.placeholder.com/600/92c952',
-      thumbnailUrl: 'https://via.placeholder.com/150/92c952',
-    },
-    {
-      albumId: 1,
-      id: 2,
-      title: 'reprehenderit est deserunt velit ipsam',
-      url: 'https://via.placeholder.com/600/771796',
-      thumbnailUrl: 'https://via.placeholder.com/150/771796',
-    },
-    {
-      albumId: 1,
-      id: 3,
-      title: 'officia porro iure quia iusto qui ipsa ut modi',
-      url: 'https://via.placeholder.com/600/24f355',
-      thumbnailUrl: 'https://via.placeholder.com/150/24f355',
-    },
-    {
-      albumId: 1,
-      id: 4,
-      title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-      url: 'https://via.placeholder.com/600/d32776',
-      thumbnailUrl: 'https://via.placeholder.com/150/d32776',
-    },
-    {
-      albumId: 1,
-      id: 5,
-      title: 'natus nisi omnis corporis facere molestiae rerum in',
-      url: 'https://via.placeholder.com/600/f66b97',
-      thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
-    },
-  ]
+  const { data, error, isLoading } = useGetPhotosQuery(id)
 
   return (
     <AccordionItem>
       <h2>
-        <AccordionButton onClick={() => console.log('Hi', id)}>
+        <AccordionButton>
           <Box flex="1" textAlign="left">
             {title}
           </Box>
@@ -53,9 +18,11 @@ const AlbumItem = ({ id, title }) => {
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        <Grid templateColumns="repeat(auto-fill, minmax(110px, 1fr))">
-          {images &&
-            images.map((image) => {
+        <Grid templateColumns="repeat(auto-fill, minmax(50px, 1fr))">
+          {error && error.message}
+          {isLoading && <Spinner />}
+          {data &&
+            data.map((image) => {
               return <AlbumThumbnail key={image.id} url={image.url} thumbnailUrl={image.thumbnailUrl} />
             })}
         </Grid>
