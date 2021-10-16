e727d154b5124b7220eddeb60b465ffdb3f8461d
abdmmar <opowae909@gmail.com>
2021-10-17 00:12:42 +0700

    fix(albums): change to use `AlbumIItem` directly and get data

--- src/features/albums/AlbumList.js
+++ src/features/albums/AlbumList.js
@@ -1,16 +1,27 @@
-import PropTypes from 'prop-types'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { Accordion } from '@chakra-ui/react'
 
-const AlbumList = ({ children }) => {
+import AlbumItem from './AlbumItem'
+import { useLazyGetAlbumsQuery } from './albumsAPI'
+import { selectUserId } from 'features/users/usersSlice'
+
+const AlbumList = () => {
+  const userId = useSelector(selectUserId)
+  const [trigger, { data }] = useLazyGetAlbumsQuery(userId)
+
+  useEffect(() => {
+    trigger(userId)
+  }, [userId])
+
   return (
     <Accordion defaultIndex={[0]} allowMultiple>
-      {children}
+      {data &&
+        data.map((album) => {
+          return <AlbumItem key={album.id} id={album.id} title={album.title} />
+        })}
     </Accordion>
   )
-}
-
-AlbumList.propTypes = {
-  children: PropTypes.node,
 }
 
 export default AlbumList
