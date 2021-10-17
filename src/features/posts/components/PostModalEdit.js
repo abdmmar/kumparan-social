5259d13e38597c8c7cc38099ba3de2e8be026345
abdmmar <opowae909@gmail.com>
2021-10-17 16:07:54 +0700

    feat(post): make edit modal as seperate component

---
+++ src/features/posts/components/PostModalEdit.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+
+import PostEditForm from './PostEditForm'
+
+const PostModalEdit = ({ data, isOpen, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader marginRight="5">{data.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <PostEditForm data={data} />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+PostModalEdit.propTypes = {
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+  data: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    body: PropTypes.string,
+  }),
+}
+
+export default PostModalEdit
